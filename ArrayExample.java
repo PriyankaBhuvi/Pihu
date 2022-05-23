328d0655e0f96b53e59e5b150536e03827829d2c
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-23 22:08:44 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,35 @@
+package com.vstl.javatraining;
+
+public class ArrayExample {
+	
+  public void getPrimeNumberData() {
+		
+		int intPrimeNumber[] = { 2,3,5,7,11};
+		System.out.println("prime number data : "+intPrimeNumber[3]);
+		
+	}
+		
+   //  Multi-Dimensional Array :
+    	 
+      public void getArrayData() {
+    	  int [] [] marks;
+    	 
+    	   marks = new int [2][3];
+    	    marks[0][0] =110;
+    	    marks[0][1] =210;
+    	    marks[0][2] =310;
+    	    marks[1][0] =410;
+    	    marks[1][1] =510;
+    	    marks[1][2] =610;
+    	    
+    	    System.out.println("printing the two dimensional array using for loop");
+    	    
+    	    for( int i =0;i<marks.length;i++);
+    	    System.out.println(1);
+    	   
+    	  
+    			  
+      }
+    	
+    }
+
