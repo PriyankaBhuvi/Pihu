c88c62761584b9cb50a9c8b9df2d8e63e98bf5cc
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-23 22:08:05 +0530

    Add files via upload

---
+++ TestLoops.java
@@ -0,0 +1,14 @@
+package com.vstl.loops;
+
+public class TestLoops {
+
+	public static void main(String[] args) {   
+		 
+		Loops objTestLoops =new Loops();
+		 objTestLoops.findBelguamCollegeNamesUsingEachLoop();
+		 
+		 
+		 objTestLoops.findTrainningEmloyeeListUsingDoWhileLoop();	
+	}
+
+}
