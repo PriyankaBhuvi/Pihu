e9c21732839dd7e9065306f6ebf1d6bde351dd2c
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-23 22:09:07 +0530

    Add files via upload

---
+++ TestArrayExample.java
@@ -0,0 +1,21 @@
+package com.vstl.javatraining;
+
+public class TestArrayExample {
+
+	public static void main(String[] args) {
+		
+		ArrayExample objArrayExample = new ArrayExample();
+		objArrayExample.getPrimeNumberData();
+		
+		objArrayExample.getArrayData();
+	}	
+		
+}
+
+
+		
+		
+
+	
+
+
